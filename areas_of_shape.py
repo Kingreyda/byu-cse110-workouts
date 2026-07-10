f660ebfb167334ac5c186f3e51da29b7182798af
kingreyda <olaribigbeking@gmail.com>
2026-07-10 08:51:42 +0100

    Added IF conditional statement

--- areas_of_shape.py
+++ areas_of_shape.py
@@ -4,6 +4,7 @@ Purpose: Areas of shapes
 """
 
 import math
+
 #Square section.
 square_length = float(input("\nWhat is the length of a side of a square (in cm)? "))
 square_area_cm = square_length ** 2
@@ -18,14 +19,15 @@ length = float(input("\nWhat Is the length of a rectangle (in cm)? "))
 width = float(input("What is the width of the rectangle (in cm)? "))
 
 rectangle_area_cm = length * width
+rectangle_area_m = rectangle_area_cm / 10000
 
 print(f"\nThe area of the rectangle is: {rectangle_area_cm:.1f} cm^2")
-print(f"The rectangle area in meters: {rectangle_area_cm / 10000:.1f} m^2")
+print(f"The rectangle area in meters: {rectangle_area_m:.1f} m^2")
 
 
-#Circle
+#Circle section.
 radius = float(input("\nWhat is the radius of the circle? "))
-circle_area = 3.14 * (radius ** 2)
+circle_area_cm = 3.14 * (radius ** 2)
 
-print(f"\nThe area of the circle is: {circle_area:.1f} cm^2")
-print(f"\nYour circle area in meters is: {circle_area /10000} cm^2")
+print(f"\nThe area of the circle is: {circle_area_cm:.1f} cm^2")
+print(f"Your circle area in meters is: {circle_area_cm /10000} m^2")
---
+++ meal_calculator.py
@@ -0,0 +1,65 @@
+"""
+Author: Olaribigbe King David
+Purpose: Meal Calculator
+
+For my creativity, I added an optional tip feature that allows customers
+to include a tip before the final bill is calculated.
+"""
+
+#LOCALISATION SECTION
+import locale
+currency_symbol = "$"
+
+locale_value = "en_US.UTF-8"
+locale.setlocale(locale.LC_ALL, locale_value)
+
+#MEAL SECTION
+child_price = float(input("\nWhat is the price of a child's meal? "))
+adult_price = float(input("What is the price of an adult's meal? "))
+
+child_no = int(input("How many children are there? "))
+adult_no = int(input("How many adults are there? ")) 
+
+child_total = child_price * child_no
+adult_total = adult_price * adult_no 
+
+subtotal = child_total + adult_total
+subtotal_formatted = locale.currency(subtotal, grouping=True, symbol=currency_symbol)
+
+print(f"\nSub-total: {subtotal_formatted}")
+
+#TAX SECTION
+tax_rate = float(input("\nWhat is the sales tax rate? "))
+sales_tax = (tax_rate / 100) * subtotal
+
+sales_tax_formatted = locale.currency(sales_tax, grouping=True, symbol=currency_symbol)
+print(f"\nSales Tax: {sales_tax_formatted}")
+
+total = sales_tax + subtotal
+total_formatted = locale.currency(total, grouping=True, symbol=currency_symbol)
+
+print(f"Total: {total_formatted}")
+
+
+#CREATIVITY SECTION - "Tip"
+tip_amount = 0
+
+tip = input("\nWould you like to drop a tip? (yes or no): ")
+
+if tip.lower() == "yes":
+    tip_amount = float(input("\nPlease input amount: "))
+
+tip_formatted = locale.currency(tip_amount, grouping=True, symbol=currency_symbol)
+print(f"\nTip: {tip_formatted}")
+
+overall_total = total + tip_amount
+overall_total_formatted = locale.currency(overall_total, grouping=True, symbol=currency_symbol)
+
+print(f"Overall total: {overall_total_formatted}")
+
+#PAYMENT SECTION
+payment_amount = float(input("\nWhat is the payment amount? "))
+change = payment_amount - overall_total
+
+change_formatted = locale.currency(change, grouping=True, symbol=currency_symbol)
+print(f"\nChange: {change_formatted}")
